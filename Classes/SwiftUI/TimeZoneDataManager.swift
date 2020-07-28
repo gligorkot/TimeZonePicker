c5391072e26905a0bd252b471a5d97ada5aca28c
Malcolm Anderson <malcolminyo@gmail.com>
2020-07-27 18:31:56 -0700

    Did some print statement cleanup

--- Classes/SwiftUI/TimeZoneDataManager.swift
+++ Classes/SwiftUI/TimeZoneDataManager.swift
@@ -14,38 +14,33 @@ class TimeZoneDataManager {
     var timeZones: [CityCountryTimeZone] = []
     
     private init() {
-        print("Initialize TimeZoneDataManager...")
         self.loadTimeZoneData()
     }
     
     func loadTimeZoneData() {
         // copied from TimeZonePickerDataSource.swift
-        print("Load time zone data on another thread")
         DispatchQueue.global(qos: .userInitiated).async {
-            print("we're in the other thread")
             do {
                 if let file = Bundle(for: TimeZoneDataManager.self).url(forResource: "CitiesAndTimeZones", withExtension: "json") {
                     let data = try Data(contentsOf: file)
-                    print(file.absoluteString)
                     do {
                         var timeZones = try JSONDecoder().decode([CityCountryTimeZone].self, from: data)
                         timeZones.sort()
-                        print("TIMEZONE - sorted!")
                         DispatchQueue.main.async {
                             self.timeZones = timeZones
-                            print("TIMEZONE - list is set, we should be all good :)")
+                            print("TimeZoneDataManager - setup is complete")
                         }
                     } catch {
                         // should never get here / invalid json
-                        print("TIMEZONE ERROR - json was invalid")
+                        print("TimeZoneDataManager - invalid JSON")
                     }
                 } else {
                     // should never get here / file does not exist
-                    print("TIMEZONE ERROR - file doesn't exist")
+                    print("TimeZoneDataManager - JSON file doesn't exist")
                 }
             } catch {
                 // should never get here / unless Data or JSONSerialization throw an error
-                print("TIMEZONE ERROR - Data, JSONSerialization threw error")
+                print("TimeZoneDataManager - Data or JSONSerialization threw error")
             }
         }
     }
